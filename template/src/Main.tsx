8f6507323269f3b733bda9ee40e34e99a0b86cc5
Herve EKPE <herve.ekpe@gmail.com>
2022-08-26 11:11:28 +0000

    add dev setup

---
+++ template/src/Main.tsx
@@ -0,0 +1,25 @@
+/**
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text> Main </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Main;
