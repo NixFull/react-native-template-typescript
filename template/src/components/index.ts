9161b3ceee1c3e832a5d17bfbfab597403e0adee
Herve EKPE <herve.ekpe@gmail.com>
2022-08-26 12:14:22 +0000

    add styles
    - Custom input
    - Common styles

---
+++ template/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Stepper } from './stepper/Stepper';
+export { default as PhoneNumberInput } from './PhoneNumberInput';
+export { default as TextInput } from './CustomTextInput';
+export { default as TouchableItem } from './TouchableItem';
